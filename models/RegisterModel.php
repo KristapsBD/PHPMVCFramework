fe5466abf1993e152455b32c081db0ce48bb9bf9
Kristaps Briks <kristaps.briks@inbox.lv>
2022-08-19 21:37:51 +0300

    Implement validation using models

--- models/RegisterModel.php
+++ models/RegisterModel.php
@@ -16,4 +16,15 @@ class RegisterModel extends Model
     {
         return 'Creating new user';
     }
+
+    public function rules(): array
+    {
+        return[
+            'firstname' => [self::RULE_REQUIRED],
+            'lastname' => [self::RULE_REQUIRED],
+            'email' => [self::RULE_REQUIRED, self::RULE_EMAIL],
+            'password' => [self::RULE_REQUIRED, [self::RULE_MIN, 'min' => 8], [self::RULE_MAX, 'max' => 24]],
+            'confirmpassword' => [self::RULE_REQUIRED, [self::RULE_MATCH, 'match' => 'password']],
+        ];
+    }
 }
