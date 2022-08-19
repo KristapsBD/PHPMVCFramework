8c94e4b4fda42d66fb8356bc4101cdce9380f0cd
Kristaps Briks <kristaps.briks@inbox.lv>
2022-08-19 15:56:25 +0300

    Define methods for routing

--- core/Router.php
+++ core/Router.php
@@ -4,7 +4,14 @@ namespace app\core;
 
 class Router
 {
+    public Request $request;
     protected array $routes = [];
+
+    public function __construct(\app\core\Request $request)
+    {
+        $this->request = $request;
+    }
+
     public function get($path, $callback)
     {
         $this->routes['get'][$path] = $callback;   
@@ -12,6 +19,13 @@ class Router
 
     public function resolve()
     {
-        var_dump($_SERVER);
+        $path = $this->request->getPath();
+        $method = $this->request->getMethod();
+        $callback = $this->routes[$method][$path] ?? false;
+        if ($callback == false){
+            echo "Not found!";
+            exit;
+        }
+        echo call_user_func($callback);
     }
 }
