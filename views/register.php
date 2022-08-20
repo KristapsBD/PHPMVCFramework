6cf6ac9565b214e697220da7ee5abcfa746f9589
Kristaps Briks <kristaps.briks@inbox.lv>
2022-08-20 13:04:24 +0300

    Implement form widget

--- views/register.php
+++ views/register.php
@@ -1,44 +1,17 @@
 <h1>Create an account</h1>
 
 <?php $form = \app\core\form\Form::begin('', "post") ?>
-    <?php echo $form->field($model, 'firstname')?>
-    <?php echo $form->field($model, 'lastname')?>
+    <div class="row">
+        <div class="col">
+            <?php echo $form->field($model, 'firstname')?>
+        </div>
+        <div class="col">
+            <?php echo $form->field($model, 'lastname')?>
+        </div>
+    </div>
     <?php echo $form->field($model, 'email')?>
-    <?php echo $form->field($model, 'password')?>
-    <?php echo $form->field($model, 'confirmpassword')?>
+    <?php echo $form->field($model, 'password')->passwordField()?>
+    <?php echo $form->field($model, 'confirmpassword')->passwordField()?>
 
     <button type="submit" class="btn btn-primary">Submit</button>
 <?php \app\core\form\Form::end() ?>
-<form action="" method="post">
-    <div class="row">
-        <div class="col">
-            <div class="mb-3">
-                <label>First Name</label>
-                <input type="text" name="firstname" value="<?php echo $model->firstname ?>"
-                class="form-control <?php echo $model->hasError('firstname') ? 'is-invalid' : ''?>">
-                <div class="invalid-feedback">
-                    <?php echo $model->getFirstError('firstname') ?>
-                </div>
-            </div>
-        </div>
-        <div class="col">
-            <div class="mb-3">
-                <label>Last Name</label>
-                <input type="text" name="lastname" class="form-control">
-            </div>
-        </div>
-    </div>
-    <div class="mb-3">
-        <label>Email</label>
-        <input type="text" name="email" class="form-control">
-    </div>
-    <div class="mb-3">
-        <label>Password</label>
-        <input type="password" name="password" class="form-control">
-    </div>
-    <div class="mb-3">
-        <label>Confirm Password</label>
-        <input type="password" name="confirmpassword" class="form-control">
-    </div>
-    <button type="submit" class="btn btn-primary">Submit</button>
-</form>
