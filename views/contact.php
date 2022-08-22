29c92bc3e59ec12e27d390d4236d88ad3d6dec40
Kristaps Briks <kristaps.briks@inbox.lv>
2022-08-22 17:44:51 +0300

    Improve form widget with abstraction

--- views/contact.php
+++ views/contact.php
@@ -1,23 +1,16 @@
 <?php
 
+use app\core\form\TextAreaField;
+
 $this->title = 'Contact';
 
 ?>
 
 <h1>Contact</h1>
 
-<form action="" method="post">
-    <div class="mb-3">
-        <label>Subject</label>
-        <input type="text" name="subject" class="form-control">
-    </div>
-    <div class="mb-3">
-        <label>Email</label>
-        <input type="text" name="email" class="form-control">
-    </div>
-    <div class="mb-3">
-        <label>Body</label>
-        <input type="text" name="body" class="form-control">
-    </div>
-    <button type="submit" class="btn btn-primary">Submit</button>
-</form>
+<?php $form = \app\core\form\Form::begin('', 'post') ?>
+<?php echo $form->field($model, 'subject') ?>
+<?php echo $form->field($model, 'email') ?>
+<?php echo new TextAreaField($model, 'body') ?>
+<button type="submit" class="btn btn-primary">Submit</button>
+<?php \app\core\form\Form::end(); ?>
